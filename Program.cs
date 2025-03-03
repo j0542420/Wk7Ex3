4d9890349733de94a590ffb375aa94fdf5e12ace
Jose Villa <j0542420@amarillocollege.com>
2025-03-02 21:10:08 -0600

    finish

--- Program.cs
+++ Program.cs
@@ -14,51 +14,110 @@ namespace Wk7Ex3
             string ProductName;
             double Price;
 
-            
+            public Product(string productID, string productName, double price)
+            { 
+                ProductID = productID;
+                ProductName = productName;
+                Price = price;
+            }
+
+            public string GetProductID()
+            {
+                return ProductID;
+            }
+
+            public string GetProductName()
+            {
+                return ProductName;
+            }
+
+            public double GetPrice()
+            {
+                return Price;
+            }
         }
 
         public class ShoppingCart
         {
             // List of products
-            static List<int> Items = new List<int>();
+            static List<Product> items = new List<Product>();
 
             // method to add product to the cart
             public void AddProduct(Product product)
-            { 
-            
+            {
+                items.Add(product);
+                Console.WriteLine(product.GetProductName() + " has been added to your cart.");
             }
             // method to remove item from cart by its id
             public void RemoveProduct(string productid)
             {
-            
+                Product productRemove = null;
+                // Find product by ID
+                foreach (var product in items)
+                {
+                    if (product.GetProductID() == productid)
+                    {
+                        productRemove = product;
+                        break;
+                    }
+                }
+
+                if (productRemove != null)
+                {
+                    items.Remove(productRemove);
+                    Console.WriteLine(productRemove.GetProductName() + " has been removed from your cart.");
+                }
+                else
+                {
+                    Console.WriteLine("Product not found in the cart.");
+                }
             }
             // method to return the total price of the products in the cart
             public void CalculateTotalPrice()
             { 
-            
+                double totalPrice = 0;
+
+                foreach (var product in items)
+                {
+                    totalPrice += product.GetPrice();
+                }
+                Console.WriteLine("Total price of items in your cart: $" + totalPrice);
             }
         }
         static void Main(string[] args)
         {
+            ShoppingCart cart = new ShoppingCart();
             while (true)
             {
                 Console.WriteLine("Welcome to the online shopping cart");
                 Console.WriteLine("1 - Add a product to your list");
                 Console.WriteLine("2 - Remove a product from your list");
                 Console.WriteLine("3 - Calculate the total of your cart");
+                Console.WriteLine("4 - Exit");
+                Console.WriteLine("Enter your choice");
 
                 string choice = Console.ReadLine();
 
                 switch (choice)
                 {
                     case "1":
-                        AddProduct(Product product);
+                        Console.WriteLine("Enter Product ID: ");
+                        string id = Console.ReadLine();
+                        Console.WriteLine("Enter Product Name: ");
+                        string name = Console.ReadLine();
+                        Console.WriteLine("Enter Product Price: ");
+                        double price = Convert.ToDouble(Console.ReadLine());
+
+                        Product newProduct = new Product(id, name, price);
+                        cart.AddProduct(newProduct);
                             break;
                     case "2":
-                        RemoveProduct(string productid);
+                        Console.WriteLine("Enter Product ID to Remove: ");
+                        string productid = Console.ReadLine();
+                        cart.RemoveProduct(productid);
                             break;
                     case "3":
-                        CalculateTotalPrice();
+                        cart.CalculateTotalPrice();
                             break;
                     case "4":
                         Console.WriteLine("Thanks for ordering from us");
