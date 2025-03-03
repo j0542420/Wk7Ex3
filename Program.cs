120dbb2ddd0d23dc3ba0d420b3ac232edd5e2bd1
Jose Villa <j0542420@amarillocollege.com>
2025-03-02 20:12:10 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wk7Ex3
+{
+    internal class Program
+    {
+        public class Product
+        {
+            string ProductID;
+            string ProductName;
+            double Price;
+
+            
+        }
+
+        public class ShoppingCart
+        {
+            // List of products
+            static List<int> Items = new List<int>();
+
+            // method to add product to the cart
+            public void AddProduct(Product product)
+            { 
+            
+            }
+            // method to remove item from cart by its id
+            public void RemoveProduct(string productid)
+            {
+            
+            }
+            // method to return the total price of the products in the cart
+            public void CalculateTotalPrice()
+            { 
+            
+            }
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Welcome to the online shopping cart");
+                Console.WriteLine("1 - Add a product to your list");
+                Console.WriteLine("2 - Remove a product from your list");
+                Console.WriteLine("3 - Calculate the total of your cart");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        AddProduct(Product product);
+                            break;
+                    case "2":
+                        RemoveProduct(string productid);
+                            break;
+                    case "3":
+                        CalculateTotalPrice();
+                            break;
+                    case "4":
+                        Console.WriteLine("Thanks for ordering from us");
+                        return;
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Wk7Ex3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Wk7Ex3")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f8cc21e1-a5b6-4dfd-88cc-a5d4ea784dc4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
