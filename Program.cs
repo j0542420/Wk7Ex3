4abd7b35d48f5412856f8603a41f94d1d523c390
Jose Villa <j0542420@amarillocollege.com>
2025-03-02 21:38:39 -0600

    finish comments

--- Program.cs
+++ Program.cs
@@ -10,10 +10,12 @@ namespace Wk7Ex3
     {
         public class Product
         {
+            //properties
             string ProductID;
             string ProductName;
             double Price;
 
+            //constroctur
             public Product(string productID, string productName, double price)
             { 
                 ProductID = productID;
@@ -21,16 +23,19 @@ namespace Wk7Ex3
                 Price = price;
             }
 
+            //method to return ID
             public string GetProductID()
             {
                 return ProductID;
             }
 
+            //method to return Name
             public string GetProductName()
             {
                 return ProductName;
             }
 
+            //method to return Price
             public double GetPrice()
             {
                 return Price;
@@ -79,6 +84,7 @@ namespace Wk7Ex3
 
                 foreach (var product in items)
                 {
+                    //calculates the total price of items
                     totalPrice += product.GetPrice();
                 }
                 Console.WriteLine("Total price of items in your cart: $" + totalPrice);
@@ -86,9 +92,13 @@ namespace Wk7Ex3
         }
         static void Main(string[] args)
         {
+            // calling for instance of ShoppingCart
             ShoppingCart cart = new ShoppingCart();
+
+            //loop for the whole menu and their choices
             while (true)
             {
+                //showing menu and the options the user can input
                 Console.WriteLine("Welcome to the online shopping cart");
                 Console.WriteLine("1 - Add a product to your list");
                 Console.WriteLine("2 - Remove a product from your list");
@@ -96,27 +106,42 @@ namespace Wk7Ex3
                 Console.WriteLine("4 - Exit");
                 Console.WriteLine("Enter your choice");
 
+                //user inputs their choice
                 string choice = Console.ReadLine();
 
+                //different options that user can pick
                 switch (choice)
                 {
                     case "1":
+                        //asking user input Product ID
                         Console.WriteLine("Enter Product ID: ");
+                        //user inputs ID
                         string id = Console.ReadLine();
+
+                        //asking user input for Product Name
                         Console.WriteLine("Enter Product Name: ");
+                        //user inputs Name of Product
                         string name = Console.ReadLine();
+
+                        //asking user for input Product Price
                         Console.WriteLine("Enter Product Price: ");
+                        //user inputs the price
                         double price = Convert.ToDouble(Console.ReadLine());
 
+                        //adding ID,Name, and Price to the List
                         Product newProduct = new Product(id, name, price);
                         cart.AddProduct(newProduct);
                             break;
                     case "2":
+                        //asking user to input a Product ID to remove from the List
                         Console.WriteLine("Enter Product ID to Remove: ");
+                        //user inputs the Product they want to remove
                         string productid = Console.ReadLine();
+                        //removes the Product from the List
                         cart.RemoveProduct(productid);
                             break;
                     case "3":
+                        //Showing the total price of items in the cart
                         cart.CalculateTotalPrice();
                             break;
                     case "4":
